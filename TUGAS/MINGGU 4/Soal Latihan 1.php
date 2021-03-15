92580460bf20c0749e1bf5e91b7b8b5dcea17c2b
Nih Latulia Rohmah Tiyas <80399915+Nihlatulia98@users.noreply.github.com>
2021-03-15 20:40:13 +0700

    Add files via upload

---
+++ TUGAS/MINGGU 4/Soal Latihan 1.php
@@ -0,0 +1,32 @@
+<?php  
+
+//Author Nih Latulia R.T - E31190397 - GOL A
+
+//membuat class mobil
+class mobillengkap {
+
+	public function nonton_tv()
+	{
+		return "Tv di hidupkan. mencari channel<br>Tv menampilkan gambar. ";
+	}
+}
+ class mobilBMW extends mobillengkap{
+
+ 	public function hidupkanmobil()
+ 	{
+ 		$pesan = "nyalakan mobil<br>";
+ 		return $pesan . $this -> nonton_tv();
+ 	}
+ 	public function matikanmobil()
+ 	{
+ 		return "matikan mobil";
+ 	}
+ 	public function ubahgigi()
+ 	{
+ 		return "ubah gigi";
+ 	}
+ }
+
+ $bmw = new mobilbmw();
+ echo $bmw -> hidupkanmobil();
+?>
